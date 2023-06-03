c59f6324a29178d3f46598748861fc3e38dd9559
jeriraooo <jerirao@foxmail.com>
2023-06-03 16:50:48 -0400

    Initial Commit

---
+++ EmojiLovers/EmojiLoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoversApp.swift
+//  EmojiLovers
+//
+//  Created by Jeri Rao on 2023/6/3.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
